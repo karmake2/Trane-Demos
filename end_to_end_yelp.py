cab8a5732c087476944210b3a902371bca55896d
Alex Nordin <anordin@mit.edu>
2018-04-02 11:59:07 -0400

    Updated Demos

--- end_to_end_yelp.py
+++ end_to_end_yelp.py
@@ -41,22 +41,32 @@ def generate_probs_and_nl(entity_id_column,
         if idx + 1 == NUM_PROBLEMS_TO_GENERATE:
             break
     prediction_problems_json = trane.prediction_problems_to_json_file(
-        probs, table_meta, entity_id_column, label_generating_column, time_column, "prediction_problems.json")
+        probs, table_meta, entity_id_column, label_generating_column, time_column, "JSON Files/yelp_prediction_problems.json")
 
     nl_descrips = trane.generate_nl_description(
-        probs, table_meta, entity_id_column, label_generating_column, time_column, trane.ConstantIntegerCutoffTimes(0))
+        probs, table_meta, entity_id_column, label_generating_column, time_column, trane.ConstantCutoffTime(0, 0))
     return probs, nl_descrips
 def convert(str, format = None):
     return datetime.datetime.strptime(str, format)
 def file_to_table_meta(filepath):
     return trane.TableMeta(json.loads(open(filepath).read()))
 
-entity_id_column = load_obj('entity_id_column')
 merged_df = load_obj('merged_df')
-entity_to_data_dict = load_obj('entity_to_data_dict')
+
+table_meta = file_to_table_meta(path_to_datasets + "meta.json")
+entity_id_column = 'business_id'
+label_generating_column = 'stars' #stars_x not stars because there are two columns with the name stars due to the merge
+time_column = 'date'
+filter_column = 'user_id'
+
+
+NUM_PROBLEMS_TO_GENERATE = 50
+probs, nl_descrips = generate_probs_and_nl(
+                            entity_id_column, label_generating_column, 
+                            time_column, table_meta, filter_column)
 
 labeler = trane.Labeler()
 entity_to_data_dict = trane.df_group_by_entity_id(merged_df, entity_id_column)
 cutoff_time = datetime.date(2014, 1, 1)
-entity_to_data_and_cutoff_dict = trane.ConstantDatetimeCutoffTime(cutoff_time).generate_cutoffs(entity_to_data_dict)
+entity_to_data_and_cutoff_dict = trane.ConstantCutoffTime(cutoff_time, cutoff_time).generate_cutoffs(entity_to_data_dict)
 labels = labeler.execute(entity_to_data_and_cutoff_dict, "JSON Files/yelp_prediction_problems.json")
